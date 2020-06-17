655d67d925c544f617b3be83fb26a902e13ea032
Anthony Martino <anthonym225@gmail.com>
2020-06-17 12:34:38 -0400

    Update

---
+++ Handwritten Digit Identifier/identify.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pt
+from sklearn.tree import DecisionTreeClassifier
+
+df = pd.read_csv("Handwritten Digit Identifier/train.csv")
+print(df)
